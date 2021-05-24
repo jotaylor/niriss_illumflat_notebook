e6146e5163523e2fddbb4ddcdcd7c2300558f09d
Jo Taylor <jotaylor@stsci.edu>
2021-05-24 15:14:09 -0400

    Set input and output dirs for rehearsal. Override distortion file. Comment out argparse for the moment, it needs more work

--- calibrate_data.py
+++ calibrate_data.py
@@ -1,4 +1,5 @@
 # Code originally from Tony Sohn
+import datetime
 import argparse
 import os
 import glob
@@ -12,12 +13,17 @@ from jwst.pipeline import Image3Pipeline
  
 os.environ['MIRAGE_DATA'] = '/ifs/jwst/wit/mirage_data'
 os.environ['PYSYN_CDBS']  = '/grp/hst/cdbs'
+now = datetime.datetime.now()
+today = now.strftime('%d%b%y')
 
+# !!!!!!!!!!!!!!!!!!!!
+## USER INPUT VARIABLES
 # Define input variables if not using argparse
-DATA_DIR = "/user/jotaylor/git/jo_niriss_illumflat_notebook/niriss_illumflat_notebook/indir"
+DATA_DIR = "/ifs/jwst/wit/niriss/cap_simulations/nis011a"
 DET1FILES = glob.glob(os.path.join(DATA_DIR, "*uncal.fits"))
 N_PROCS = None
-OUTDIR = "/user/jotaylor/git/jo_niriss_illumflat_notebook/niriss_illumflat_notebook/out"
+OUTDIR = f"/ifs/jwst/wit/niriss/cap_simulations/nis011a/out_{today}"
+# !!!!!!!!!!!!!!!!!!!!
 
 # Modify Image2 parameter ref file to skip photom and resample
 step = Image2Pipeline()
@@ -26,9 +32,11 @@ af = asdf.open("calwebb_image2.asdf")
 for i,dct in enumerate(af.tree["steps"]):
     if dct["name"] in ["photom", "resample", "background"]:
         af.tree["steps"][i]["parameters"]["skip"] = True
+    if dct["name"] == "assign_wcs":
+        af.tree["steps"][i]["parameters"]["override_distortion"] = "/grp/crds/cache/references/jwst/jwst_niriss_distortion_0010.asdf"
 new_config = os.path.join(OUTDIR, "calwebb_image3.asdf")
 af.write_to(new_config)
-
+af.close()
 CONFIG_FILES = {"Detector1Pipeline": None, "Image2Pipeline": new_config} 
 
 
@@ -43,6 +51,8 @@ def _make_pipeline(pipeline, outdir, config_file, raw_file):
 
 # Run the pipeline in parallel
 def parallelize_pipeline(det1files, n_procs=N_PROCS, outdir=OUTDIR, config_files=CONFIG_FILES):
+    if not os.path.exists(outdir):
+        os.path.mkdir(outdir)
     if n_procs is None:
         n_procs = multiprocessing.cpu_count()
     with multiprocessing.Pool(n_procs) as pool:
@@ -57,13 +67,14 @@ def parallelize_pipeline(det1files, n_procs=N_PROCS, outdir=OUTDIR, config_files
 
  
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--datadir", 
-                        help="Directory with uncal files to calibrate")
-    parser.add_argument("-o", "--outdir", 
-                        help="Calibrated file output directory")
-    parser.add_argument("-c", "--cpu", 
-                        help="CPU count for multiprocessing")
-    args = parser.parse_args()
-    files = glob.glob(os.path.join(args.datadir, "*uncal.fits"))
-    parallelize_pipeline(files, n_procs = args.cpu, outdir=args.outdir)
+    parallelize_pipeline(DET1FILES)
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument("-d", "--datadir", default=DATA_DIR,
+#                        help="Directory with uncal files to calibrate")
+#    parser.add_argument("-o", "--outdir", default=OUTDIR,
+#                        help="Calibrated file output directory")
+#    parser.add_argument("-c", "--cpu", default=N_PROCS, 
+#                        help="CPU count for multiprocessing")
+#    args = parser.parse_args()
+#    files = glob.glob(os.path.join(args.datadir, "*uncal.fits"))
+#    parallelize_pipeline(files, n_procs=args.cpu, outdir=args.outdir)
